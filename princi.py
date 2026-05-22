b6d858ac4d66ab164aa9cc16457ce468b5189a35
Fraaaaaaaaaaaaaaaaaaaan <frmuuoo@gmail.com>
2026-05-22 09:46:10 -0300

    Iniciada la pantalla

--- princi.py
+++ princi.py
@@ -2,9 +2,12 @@ import pygame
 import sys
 from pygame.locals import *
 largo=400#la cambiaremos a la pantalla la vdd que no me se los parametros
-ancho=900
+ancho=400
 pygame.init() 
 pantalla=pygame.display.set_mode((largo,ancho))
+pygame.display.set_caption("ALPHA CENTAURI")
+icono=pygame.image.load(r"C:\Users\frmuu\Downloads\star.ico")
+pygame.display.set_icon(icono)
 while True:
     for evento in pygame.event.get():
         if evento.type==QUIT:
